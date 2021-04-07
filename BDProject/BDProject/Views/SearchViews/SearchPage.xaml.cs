0482cd0eca75c3f11e89265dfdd1bed7456e967d
Daniel Kostov <45979039+DdanielRrKk@users.noreply.github.com>
2021-04-07 21:40:12 +0300

    Emoji Update
    - Found encoding for emojis, now they can be saved

--- BDProject/BDProject/Views/SearchViews/SearchPage.xaml.cs
+++ BDProject/BDProject/Views/SearchViews/SearchPage.xaml.cs
@@ -2,9 +2,11 @@
 using BDProject.ViewModels.SearchViewModels;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -21,12 +23,25 @@ namespace BDProject.Views.SearchViews
             BindingContext = new SearchPageViewModel();
         }
 
-        private async void Searcher_TextChangedAsync(object sender, TextChangedEventArgs e)
+        private void Searcher_TextChangedAsync(object sender, TextChangedEventArgs e)
         {
-            if (EmojiHandler.HasUnsoppertedCharacter(Searcher.Text))
+            if (string.IsNullOrEmpty(Searcher.Text)) { return; }
+
+            UTF8Encoding utf8 = new UTF8Encoding(true, true);
+            Byte[] encodedBytes = utf8.GetBytes(Searcher.Text);
+
+            string encodedBytesString = "";
+            for(int i=0; i<encodedBytes.Length; i++)
             {
-                await App.Current.MainPage.DisplayAlert("===TEST===", "Has Emoji", "OK");
+                if (i == 0) { encodedBytesString = encodedBytesString + encodedBytes[i]; }
+
+                encodedBytesString = encodedBytesString + " " + encodedBytes[i];
             }
+
+            emojicode.Text = encodedBytesString;
+
+            String decodedString = utf8.GetString(encodedBytes);
+            emojiicon.Text = decodedString;
         }
     }
 }
